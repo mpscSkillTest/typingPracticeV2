d44321ef2db76440bdcd357d299d37b639cbfc17
yogeshKulkarni20 <thisisyogeshkulkarni@gmail.com>
2024-05-25 12:25:36 +0530

    feat(results): updated info message icons

---
+++ frontend/src/components/Dashboard/shared/PassingInfoMessage.tsx
@@ -0,0 +1,39 @@
+import { Icons } from "@/components/ui/icons";
+import type { Subject } from "../../../types";
+
+type Props = {
+  subject?: Subject;
+  shouldShowInfo?: boolean;
+};
+
+const PassingInfoMessage = ({ subject, shouldShowInfo }: Props) => {
+  if (!shouldShowInfo) {
+    return null;
+  }
+
+  let minimumKeystrokes = 1500;
+  if (subject === "ENGLISH") {
+    minimumKeystrokes = 2000;
+  }
+
+  return (
+    <div className="flex flex-col gap-2 my-2 text-xs font-medium">
+      <div className="flex gap-1 items-center">
+        <Icons.BadgeInfo height={14} width={14} />
+        Exam Passing criteria
+      </div>
+      <div className="flex flex-col gap-2">
+        <span>
+          <b>Open Category</b>: Minimum {minimumKeystrokes} Keystrokes and
+          Accuracy greater than or equal to 93%
+        </span>
+        <span>
+          <b>Reserved Category</b>: Minimum {minimumKeystrokes} Keystrokes and
+          Accuracy greater than or equal to 90%
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default PassingInfoMessage;
